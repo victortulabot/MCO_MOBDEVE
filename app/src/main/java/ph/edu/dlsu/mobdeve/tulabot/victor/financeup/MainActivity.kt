92099ffe32d77d3ebf9ddb6fa94d78d00f2156c2
Victor Tulabot <44962471+victortulabot@users.noreply.github.com>
2021-11-22 09:43:43 +0800

    Initial Commit

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/tulabot/victor/financeup/MainActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.mobdeve.tulabot.victor.financeup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
